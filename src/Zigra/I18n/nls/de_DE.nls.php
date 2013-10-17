acff9105c13c0a6b9826453ee664f3d0e0b6016f
Manuel Dalla Lana <endelwar@aregar.it>
2013-10-17 11:55:22 +0200

    adding de_DE nls file

---
+++ src/Zigra/I18n/nls/de_DE.nls.php
@@ -0,0 +1,19 @@
+<?php
+return array(
+// Native language name
+    'language' => array('de_DE' => 'Deutsch'),
+    'englishlang' => array('de_DE' => 'German'),
+// Possible locale for language
+    'locale' => array('de_DE' => 'de_DE,de_DE.utf8,de_DE.utf-8,de_DE.UTF-8,de_DE@euro,deu,german,German_Germany.1252'),
+// Encoding of the language
+    'encoding' => array('de_DE' => 'UTF-8'),
+// Direction of the language
+    'direction' => array('de_DE' => 'ltr'),
+// Possible aliases for language
+    'alias' => array(
+        'de' => 'de_DE',
+        'deutsch' => 'de_DE',
+        'deu' => 'de_DE',
+        'de_DE.ISO8859-1' => 'de_DE'
+    )
+);
