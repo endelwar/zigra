928cb90aa207997127cd8ab9bfaf3408a8fff6c0
Manuel Dalla Lana <endelwar@aregar.it>
2013-10-17 11:59:06 +0200

    adding es_ES nls file

---
+++ src/Zigra/I18n/nls/es_ES.nls.php
@@ -0,0 +1,22 @@
+<?php
+return array(
+// Native language name
+    'language' => array('es_ES' => 'Espa&ntilde,ol'),
+    'englishlang' => array('es_ES' => 'Spanish'),
+// Possible locale for language
+    'locale' => array('es_ES' => 'es_ES,es_ES.utf8,es_ES.utf-8,es_ES.UTF-8,es_ES@euro,spanish,Spanish_Spain.1252'),
+// Encoding of the language
+    'encoding' => array('es_ES' => 'UTF-8'),
+// Direction of the language
+    'direction' => array('es_ES' => 'ltr'),
+// Possible aliases for language
+    'alias' => array(
+        'es' => 'es_ES',
+        'espa&ntilde,ol' => 'es_ES',
+        'eng' => 'es_ES',
+        'es_AR' => 'es_ES',
+        'es_PE' => 'es_ES',
+        'es_MX' => 'es_ES',
+        'es_US.ISO8859-1' => 'es_ES'
+    )
+);
