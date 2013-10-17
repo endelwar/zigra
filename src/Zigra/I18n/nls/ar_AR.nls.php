4169e0fc93de743373b8ebbe752fa644e00971f2
Manuel Dalla Lana <endelwar@aregar.it>
2013-10-17 12:01:32 +0200

    adding ar_AR nls file

---
+++ src/Zigra/I18n/nls/ar_AR.nls.php
@@ -0,0 +1,22 @@
+<?php
+return array(
+// Native language name
+    'language' => array('ar_AR' => 'العربية'),
+    'englishlang' => array('ar_AR' => 'Arabic'),
+// Possible locale for language
+    'locale' => array('ar_AR' => 'ar_AR,ar_AR.utf8,ar_AR.utf-8,ar_AR.UTF-8,ar_AE.utf-8,ar_AE.UTF-8,arabic,Arabic_Saudi Arabia.1256'),
+// Encoding of the language
+    'encoding' => array('ar_AR' => 'UTF-8'),
+// Direction of the language
+    'direction' => array('ar_AR' => 'rtl'),
+// Possible aliases for language
+    'alias' => array(
+        'ar' => 'ar_AR',
+        'arabic' => 'ar_AR',
+        'arb' => 'ar_AR',
+        'ar_SY' => 'ar_AR',
+        'ar_SA' => 'ar_AR',
+        'ar_AR.ISO8859-6' => 'ar_AR',
+        'ar_AR.windows-1256' => 'ar_AR'
+    )
+);
