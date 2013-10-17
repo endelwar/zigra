fc3235cc6155c072bc17d1c8781be18a7d1bf25e
Manuel Dalla Lana <endelwar@aregar.it>
2013-10-17 11:48:13 +0200

    adding I18N

---
+++ src/Zigra/I18n/nls/it_IT.nls.php
@@ -0,0 +1,21 @@
+<?php
+return array(
+// Native language name
+    'language' => array('it_IT' => 'Italiano'),
+    'englishlang' => array('it_IT' => 'Italian'),
+// Possible locale for language
+    'locale' => array('it_IT' => 'it_IT,it_IT.utf8,it_IT.utf-8,it_IT.UTF-8,it_IT@euro,italian,Italian_Italy.1252'),
+// Encoding of the language
+    'encoding' => array('it_IT' => 'UTF-8'),
+// Direction of the language
+    'direction' => array('it_IT' => 'ltr'),
+// Possible aliases for language
+    'alias' => array(
+        'it' => 'it_IT',
+        'italiano' => 'it_IT',
+        'ita' => 'it_IT',
+        'italian' => 'it_IT',
+        'it_IT.ISO8859-1' => 'it_IT',
+        'it_IT.ISO8859-15' => 'it_IT'
+    )
+);
