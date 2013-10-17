1fb97e5e467b7a57dd345741e6005a2c50f49bf6
Manuel Dalla Lana <endelwar@aregar.it>
2013-10-17 12:16:03 +0200

    adding ja_JA nls file

---
+++ src/Zigra/I18n/nls/ja_JP.nls.php
@@ -0,0 +1,20 @@
+<?php
+return array(
+// Native language name
+    'language' => array('ja_JP' => '日本語'),
+    'englishlang' => array('ja_JP' => 'Japanese'),
+// Possible locale for language
+    'locale' => array('ja_JP' => 'ja_JP,ja_JP.utf8,ja_JP.utf-8,ja_JP.UTF-8,ja_JP.SJIS,ja_JP.eucjp,japanese,Japanese_Japan.932'),
+// Encoding of the language
+    'encoding' => array('ja_JP' => 'UTF-8'),
+// Direction of the language
+    'direction' => array('ja_JP' => 'ltr'),
+// Possible aliases for language
+    'alias' => array(
+        'ja' => 'ja_JP',
+        'japanese' => 'ja_JP',
+        'ja_JP.EUC-JP' => 'ja_JP',
+        'ja_JP.Shift_JIS' => 'ja_JP',
+        'ja_JP.UTF-8' => 'ja_JP'
+    )
+);
