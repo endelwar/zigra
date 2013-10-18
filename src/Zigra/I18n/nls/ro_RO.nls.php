848ee02d4f78295b42e90f7c723898fd49b1a00d
Manuel Dalla Lana <endelwar@aregar.it>
2013-10-18 10:30:21 +0200

    adding ro_RO nls file

---
+++ src/Zigra/I18n/nls/ro_RO.nls.php
@@ -0,0 +1,21 @@
+<?php
+return array(
+// Native language name
+    'language' => array('ro_RO' => 'Română'),
+    'englishlang' => array('ro_RO' => 'Romanian'),
+// Possible locale for language
+    'locale' => array('ro_RO' => 'ro_RO,ro_RO.utf8,ro_RO.UTF-8,ro_RO.utf-8,romanian,Romanian_Romania.1250'),
+// Encoding of the language
+    'encoding' => array('ro_RO' => 'UTF-8'),
+// Direction of the language
+    'direction' => array('ro_RO' => 'ltr'),
+// Possible aliases for language
+    'alias' => array(
+        'ro' => 'ro_RO',
+        'Română' => 'ro_RO',
+        'rom' => 'ro_RO',
+        'romanian' => 'ro_RO',
+        'ro_RO.ISO8859-2' => 'ro_RO',
+        'ro_RO.ISO8859-16' => 'ro_RO'
+    )
+);
