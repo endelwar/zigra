f8b6e26df00e8d11985080d256dc3a5879e7827c
Manuel Dalla Lana <endelwar@aregar.it>
2014-08-08 13:55:17 +0200

    permit base64 encoded string as parameter value

--- src/Zigra/Route/Compiler.php
+++ src/Zigra/Route/Compiler.php
@@ -10,16 +10,14 @@ class Zigra_Route_Compiler
         $requirements = $route->GetRequirements();
         //$options = $route->GetOptions();
         $defaults = $route->GetDefaults();
-        //$len = strlen($pattern);
         $tokens = array();
         $variables = array();
         $pos = 0;
         $regex = '#';
 
-        preg_match_all('#.\{([\w\d_-]+)\}#', $pattern, $matches, PREG_OFFSET_CAPTURE | PREG_SET_ORDER);
+        preg_match_all('#.\{([\w\d\=_-]+)\}#', $pattern, $matches, PREG_OFFSET_CAPTURE | PREG_SET_ORDER);
         if (count($matches)) {
             foreach ($matches as $match) {
-                //var_dump($match);
                 if ($text = substr($pattern, $pos, $match[0][1] - $pos)) {
                     $regex .= str_replace('/', '\/', $text) . '\/';
                 }
@@ -28,7 +26,7 @@ class Zigra_Route_Compiler
                         $regex .= '(' . $requirements[$var] . ')\/';
                     } else {
                         //$regex .= '([\w\d_]+)\/';
-                        $regex .= '(?P<' . $var . '>[\w\d_-]+)\/';
+                        $regex .= '(?P<' . $var . '>[\w\d\=_-]+)\/';
                     }
                     $variables[] = $match[1][0];
                 }
