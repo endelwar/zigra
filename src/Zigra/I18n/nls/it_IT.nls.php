f73c2e18a1e38559421a4c120b07087ec6565400
Manuel Dalla Lana <endelwar@aregar.it>
2016-03-08 11:40:46 +0100

    Add "it" as locale alias for it_IT

--- src/Zigra/I18n/nls/it_IT.nls.php
+++ src/Zigra/I18n/nls/it_IT.nls.php
@@ -5,7 +5,7 @@ return array(
     'language' => array('it_IT' => 'Italiano'),
     'englishlang' => array('it_IT' => 'Italian'),
     // Possible locale for language
-    'locale' => array('it_IT' => 'it_IT,it_IT.utf8,it_IT.utf-8,it_IT.UTF-8,it_IT@euro,italian,Italian_Italy.1252'),
+    'locale' => array('it_IT' => 'it,it_IT,it_IT.utf8,it_IT.utf-8,it_IT.UTF-8,it_IT@euro,italian,Italian_Italy.1252'),
     // Encoding of the language
     'encoding' => array('it_IT' => 'UTF-8'),
     // Direction of the language
