fb29e92e55a59bd4ba33500c10300de41be145ee
Manuel Dalla Lana <manuel@pepeverde.agency>
2016-12-30 18:49:15 +0100

    Add phpunit files

---
+++ tests/Bootstrap.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * This file is part of Zigra.
+ *
+ * (c) Manuel Dalla Lana <manuel@pepeverde.agency>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+error_reporting(E_ALL);
+if (function_exists('date_default_timezone_set') && function_exists('date_default_timezone_get')) {
+    date_default_timezone_set(@date_default_timezone_get());
+}
+
+require_once __DIR__ . '/../vendor/autoload.php';
