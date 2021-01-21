ed2e2904bd68d158b2fe1880b974c7a7af08cc38
Manuel Dalla Lana <endelwar@aregar.it>
2021-01-21 19:24:42 +0100

    Inline return of function results

--- src/Zigra/Tools.php
+++ src/Zigra/Tools.php
@@ -4,8 +4,6 @@ class Zigra_Tools
 {
     public static function br2nl($string)
     {
-        $return = preg_replace('#<br[[:space:]]*/?' . '[[:space:]]*>#i', chr(13) . chr(10), $string);
-
-        return $return;
+        return preg_replace('#<br[[:space:]]*/?' . '[[:space:]]*>#i', chr(13) . chr(10), $string);
     }
 }
