a1982cf71cb66695fb5daaf4d3b8012070aa8d2a
Manuel Dalla Lana <endelwar@aregar.it>
2021-12-23 17:24:02 +0100

    Upgrade dev stack

--- rector.php
+++ rector.php
@@ -3,23 +3,25 @@
 declare(strict_types=1);
 
 use Rector\Core\Configuration\Option;
-use Rector\Php74\Rector\Property\TypedPropertyRector;
 use Rector\Set\ValueObject\SetList;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+//use Rector\Php74\Rector\Property\TypedPropertyRector;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     // get parameters
     $parameters = $containerConfigurator->parameters();
     $parameters->set(Option::PATHS, [__DIR__ . '/src']);
+
     // Define what rule sets will be applied
-    $parameters->set(Option::SETS, [
-        //SetList::DEAD_CODE,
-        SetList::PHP_56,
-        SetList::PHP_71,
-        SetList::PHP_72,
-        SetList::PHP_73,
-        SetList::PHP_74,
-    ]);
+    //$containerConfigurator->import(SetList::DEAD_CODE);
+    $containerConfigurator->import(SetList::PHP_56);
+    $containerConfigurator->import(SetList::PHP_70);
+    $containerConfigurator->import(SetList::PHP_71);
+    $containerConfigurator->import(SetList::PHP_72);
+    $containerConfigurator->import(SetList::PHP_73);
+    $containerConfigurator->import(SetList::PHP_74);
+
 
     // get services (needed for register a single rule)
     // $services = $containerConfigurator->services();
